19a6cb6cc7d3a7377038808f102115d5901e2b73
aniqatc <aniqatc97@gmail.com>
2022-11-20 00:55:03 -0500

    Fix Margin Bug ('lg' Breakpoint for Global Overview Container)

--- button.js
+++ button.js
@@ -1,23 +1,23 @@
 function changeTheme() {
 	document
-		.querySelectorAll('.local-overview, .global-overview, .search-btn')
-		.forEach((el) => el.classList.toggle('dark-container'));
+		.querySelectorAll(".local-overview, .global-overview, .search-btn")
+		.forEach((el) => el.classList.toggle("dark-container"));
 	document
-		.querySelectorAll('.daily, button')
-		.forEach((el) => el.classList.toggle('dark-hover'));
+		.querySelectorAll(".daily, button")
+		.forEach((el) => el.classList.toggle("dark-hover"));
 	document
-		.querySelectorAll('.input-group')
-		.forEach((el) => el.classList.toggle('dark-btn'));
+		.querySelectorAll(".input-group")
+		.forEach((el) => el.classList.toggle("dark-btn"));
 	document
-		.querySelectorAll('.global-item')
-		.forEach((el) => el.classList.toggle('light-hover'));
+		.querySelectorAll(".global-item")
+		.forEach((el) => el.classList.toggle("light-hover"));
 	document
-		.querySelectorAll('.card, .list-group-item, body')
-		.forEach((el) => el.classList.toggle('dark'));
+		.querySelectorAll(".card, .list-group-item, body")
+		.forEach((el) => el.classList.toggle("dark"));
 	document
-		.querySelectorAll('.list-group-item, footer, .sun-time')
-		.forEach((el) => el.classList.toggle('dark-icon'));
+		.querySelectorAll(".list-group-item, footer, .sun-time")
+		.forEach((el) => el.classList.toggle("dark-icon"));
 }
 
-let themeToggle = document.querySelector('#flexSwitchCheckChecked');
-themeToggle.addEventListener('click', changeTheme);
+let themeToggle = document.querySelector("#flexSwitchCheckChecked");
+themeToggle.addEventListener("click", changeTheme);
