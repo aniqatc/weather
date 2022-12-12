093bc5bda735168affad7487e79d502177af4cd2
aniqatc <aniqatc97@gmail.com>
2022-12-12 01:12:54 -0500

    Improve Global Temps Using Loop; Add More Data for Current Location

--- index.js
+++ index.js
@@ -80,31 +80,15 @@ function toggleTemp(event) {
 
 celsius.addEventListener("click", toggleTemp);
 
-// Search Functionality
-function searchCity(event) {
-	event.preventDefault();
-	let searchInput = document.querySelector("#search-input").value;
-	let locationHeading = document.querySelector("#location");
-	if (searchInput) {
-		locationHeading.textContent = `${searchInput}`;
-	} else {
-		alert(`Please provide a city name.`);
-	}
-}
-
-let searchBtn = document.querySelector(".search-form");
-searchBtn.addEventListener("submit", searchCity);
-
 // Current Location via Geolocation
 let apiKey = "c547e666ba264994fad007fc08810597";
 let apiUrl = "https://api.openweathermap.org/data/2.5/weather";
 let units = "imperial";
 
 let geolocationButton = document.querySelector("#geolocation-btn");
-geolocationButton.addEventListener(
-	"click",
-	navigator.geolocation.getCurrentPosition(getLocation)
-);
+geolocationButton.addEventListener("click", function () {
+	navigator.geolocation.getCurrentPosition(getLocation);
+});
 
 function getLocation(position) {
 	let lon = position.coords.longitude;
@@ -115,36 +99,58 @@ function getLocation(position) {
 		.then(displayCurrentTemperature);
 }
 
-// Display Temperature Based On GPS
+// Search Functionality
+function searchCity(event) {
+	event.preventDefault();
+	let searchInput = document.querySelector("#search-input").value;
+	let locationHeading = document.querySelector("#location");
+	if (searchInput) {
+		axios
+			.get(`${apiUrl}?q=${searchInput}&appid=${apiKey}&units=${units}`)
+			.then(displayCurrentTemperature);
+		locationHeading.textContent = `${searchInput}`;
+	} else {
+		alert(`Please provide a city name.`);
+	}
+}
+
+let searchBtn = document.querySelector(".search-form");
+searchBtn.addEventListener("submit", searchCity);
+
+// Display Temperature
 let currentTemp = document.querySelector("#temp-now");
 let highTemp = document.querySelector("#high-temp");
 let lowTemp = document.querySelector("#low-temp");
 let feelsLikeTemp = document.querySelector("#feels-like");
 let descriptionTemp = document.querySelector("#description-temp");
+let sunrise = document.querySelector("#sunrise-time");
+let sunset = document.querySelector("#sunset-time");
 
 function displayCurrentTemperature(response) {
-	console.log(response);
-	let dataTemp = response.data;
-	currentTemp.innerHTML = `${Math.round(dataTemp.main.temp)}`;
-	highTemp.innerHTML = `${Math.round(dataTemp.main.temp_max)}`;
-	lowTemp.innerHTML = `${Math.round(dataTemp.main.temp_min)}`;
-	feelsLikeTemp.innerHTML = `${Math.round(dataTemp.main.feels_like)}`;
-	descriptionTemp.innerHTML = `${dataTemp.weather[0].description}`;
+	if (response.status == 200) {
+		let dataTemp = response.data;
+		console.log(response);
+		currentTemp.innerHTML = `${Math.round(dataTemp.main.temp)}`;
+		highTemp.innerHTML = `${Math.round(dataTemp.main.temp_max)}`;
+		lowTemp.innerHTML = `${Math.round(dataTemp.main.temp_min)}`;
+		feelsLikeTemp.innerHTML = `${Math.round(dataTemp.main.feels_like)}`;
+		descriptionTemp.innerHTML = `${dataTemp.weather[0].description}`;
+		let apiSunsrise = new Date(dataTemp.sys.sunrise * 1000).toLocaleTimeString(
+			"en-US"
+		);
+		let apiSunset = new Date(dataTemp.sys.sunset * 1000).toLocaleTimeString(
+			"en-US"
+		);
+		sunrise.innerHTML = `${apiSunsrise}`;
+		sunset.innerHTML = `${apiSunset}`;
+	} else {
+		alert(`Please enter a valid city name.`);
+	}
 }
 
 // Display Temperatures for Global Forecast (Default)
-let seattleTemp = document.querySelector("#seattle-temp");
-let rabatTemp = document.querySelector("#rabat-temp");
-let englandTemp = document.querySelector("#england-temp");
-let parisTemp = document.querySelector("#paris-temp");
-let delhiTemp = document.querySelector("#delhi-temp");
-let cityTemps = [seattleTemp, rabatTemp, englandTemp, parisTemp, delhiTemp];
-let seattleDesc = document.querySelector("#seattle-description");
-let rabatDesc = document.querySelector("#rabat-description");
-let englandDesc = document.querySelector("#england-description");
-let parisDesc = document.querySelector("#paris-description");
-let delhiDesc = document.querySelector("#delhi-description");
-let cityDesc = [seattleDesc, rabatDesc, englandDesc, parisDesc, delhiDesc];
+let globalTemps = document.querySelectorAll(".global-temps");
+let globalDesc = document.querySelectorAll(".global-descriptions");
 let city = ["Seattle", "Rabat", "England", "Paris", "Delhi"];
 
 function displayGlobalTemperature() {
@@ -152,12 +158,19 @@ function displayGlobalTemperature() {
 		axios
 			.get(`${apiUrl}?q=${city[i]}&appid=${apiKey}&units=${units}`)
 			.then(function (response) {
-				cityTemps[i].innerHTML = Math.round(response.data.main.temp);
-				cityDesc[i].innerHTML = `${response.data.weather[0].description}`;
+				globalTemps[i].innerHTML = Math.round(response.data.main.temp);
+				globalDesc[i].innerHTML = `${response.data.weather[0].description}`;
 			});
 	}
 }
 
 displayGlobalTemperature();
 
-// Display Temperatures for New York (Default)
+// Display NYC Temperature (Default)
+function displayDefaultTemperature() {
+	axios
+		.get(`${apiUrl}?q=New York&appid=${apiKey}&units=${units}`)
+		.then(displayCurrentTemperature);
+}
+
+displayDefaultTemperature();
